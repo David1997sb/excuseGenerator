6426ee56f4ac632d918fa280dd170a8306646af5
David1997sb <salasbar97@gmail.com>
2022-01-15 17:02:49 +0000

    Adding excuses

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  var who = ["The dog", "My grandma", "His turtle", "My bird"];
+  var action = ["ate", "peed", "crushed", "broke"];
+  var what = ["my homework", "the keys", "the car"];
+  var when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var whoIndex = Math.floor(Math.random() * who.length);
+  var actionIndex = Math.floor(Math.random() * action.length);
+  var whatIndex = Math.floor(Math.random() * what.length);
+  var whenIndex = Math.floor(Math.random() * when.length);
+  document.getElementById("excuse").innerHTML =
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex];
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
